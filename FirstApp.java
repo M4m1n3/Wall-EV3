dc288dd9ee1343c33b05de4c938ae00e46003743
M4m1n3 <132770351+M4m1n3@users.noreply.github.com>
2023-09-11 16:59:28 +0200

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -1,12 +1,40 @@
+import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
-import lejos.hardware.lcd.GraphicsLCD;
-import lejos.utility.Delay;
-
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 
 public class FirstApp {
-	public static void main(String[] args) {
-		GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-		g.drawString("Hello World",0,0,GraphicsLCD.VCENTER | GraphicsLCD.LEFT);
-		Delay.msDelay(5000);
-	}
+
+    public static void main(String[] args) {
+        // Obtenez une instance de la brique EV3
+        Brick ev3 = BrickFinder.getDefault();
+
+        // Initialisez les moteurs des roues
+        EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+        EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+        EV3LargeRegulatedMotor bras = new EV3LargeRegulatedMotor(MotorPort.D);
+
+        // Faites tourner les roues vers l'avant pendant 2 secondes
+        bras.forward();
+        leftMotor.forward();
+        rightMotor.forward();
+
+        try {
+            Thread.sleep(5000); // Pause de 2 secondes
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Arrêtez les moteurs
+        bras.stop();
+        bras.setSpeed(8000);
+        rightMotor.setAcceleration(5000);
+        rightMotor.stop();
+        leftMotor.stop();
+
+        // Libérez les ressources
+        bras.close();
+        leftMotor.close();
+        rightMotor.close();
+    }
 }
