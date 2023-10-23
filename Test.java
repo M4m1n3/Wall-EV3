1396d00418083df8a78ce16bde4ce9d4e70b9ac1
SalomeLrch <144694518+SalomeLrch@users.noreply.github.com>
2023-10-23 13:24:56 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,33 @@
+package ia;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.*;
+import lejos.utility.Delay;
+public class Test {
+
+	public static void main(String[] args) {
+		Sensor robot = new Sensor();
+		Delay d = new Delay();
+		float s1,s2;
+		float[] s3;
+		for (int i = 0; i<30; i++) {
+			//s1 = robot.dist();
+			//s2 = robot.estTouche();
+			s3 = robot.couleur();
+			//System.out.print(s1);
+			//System.out.print(" ");
+			//System.out.print(s2);
+			//System.out.print(" ");
+			System.out.print(s3[0]);
+			System.out.print(' ');
+			System.out.print(s3[1]);
+			System.out.print(' ');
+			System.out.print(s3[2]);
+			System.out.println();
+			d.msDelay(1000);
+		}
+		d.msDelay(5000);
+	}
+}
