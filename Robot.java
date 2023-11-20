5aa774af88495900868d0f32f338ba0ab4877870
M4m1n3 <132770351+M4m1n3@users.noreply.github.com>
2023-11-20 16:56:48 +0100

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,6 +1,8 @@
 package ia;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
@@ -12,7 +14,7 @@ public class Robot {
 	Sensor sens = new Sensor();
 	Actionneurs act = new Actionneurs();
 	Delay d = new Delay();
-	
+
 	/*public void alignePaletProche() {
 		float min=999;
 		float dN=sens.dist();
@@ -33,7 +35,7 @@ public class Robot {
 		}
 			act.rotate(360-rotaDepuisMin,false);
 	}*/
-	
+
 	public void alignePaletProcheU() {
 		act.chassis.setAngularSpeed(300);
 		float min = 999;
@@ -59,16 +61,42 @@ public class Robot {
 		act.rotate(rota, false);
 		d.msDelay(10000);
 	}
+	public ArrayList<Float> getMesures() {
+		act.chassis.setAngularSpeed(300);
+		ArrayList<Float> mesures = new ArrayList<Float>();
+		float dN = sens.dist();
+		act.rotate(360,true);
+		while(act.chassis.isMoving()) {
+			mesures.add(dN);
+			dN = sens.dist();
+		}
+		return mesures;
+	}
 	
-	
+	public Float getMin(ArrayList<Float> mesures) {
+		return Collections.min(mesures);
+	}
+	public boolean diffMesures(ArrayList<Float> mesures) {
+		boolean mur = false;
+		for (int i = 0; i < mesures.size() - 1; i++) {
+			if (Math.abs(mesures.get(i) - mesures.get(i + 1)) > 0.1) {
+				mur = true;
+				break;
+			}
+		}
+		return mur;
+	}
+	//booleen sert a rien, se servir de l'arraylist pr retourner les intervalles d'angle ou y'a des murs pr les éviter
+
+
 	public boolean detectionMur() {
-        return (sens.dist()<20);
-    }
-	
+		return (sens.dist()<20);
+	}
+
 	public void eviteObstacle() {
 		if(detectionMur())act.rotate(90, false);
 	}
-	
+
 	public static void main(String[] args) {
 		Robot bot = new Robot();
 		bot.alignePaletProcheU();
